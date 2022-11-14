f7120e0c6de1a6881279086a967228504192b9cc
adrrob1002 <adrianrobinson101@gmail.com>
2022-11-14 08:32:32 +0300

    Add WoodenCrate Spawner

---
+++ src/client/interactions/spawner-interaction.ts
@@ -0,0 +1,40 @@
+import Make from "@rbxts/make";
+import { CollectionService, Workspace } from "@rbxts/services";
+import { Interaction, OnInteracted } from "client/controllers/interactions/interactions-decorator";
+import { Tag } from "types/enum/tags";
+
+@Interaction({
+	interactionId: "Spawner",
+	interactionText: (interactable) => {
+		return `Spawn ${interactable.GetAttribute("SpawnerType")}`;
+	},
+})
+export class SpawnerInteraction implements OnInteracted {
+	public onInteracted(obj: BasePart) {
+		const spawnerType = obj.GetAttribute("SpawnerType");
+		if (spawnerType === undefined) return;
+
+		switch (spawnerType) {
+			case "WoodenCrate": {
+				const crate = Make("Part", {
+					Name: "WoodenCrate",
+					Anchored: false,
+					BrickColor: new BrickColor("Brown"),
+					CanCollide: true,
+					FormFactor: Enum.FormFactor.Custom,
+					Material: Enum.Material.Wood,
+					Position: obj.Position,
+					Size: new Vector3(2, 2, 2),
+					Parent: Workspace,
+				});
+
+				CollectionService.AddTag(crate, Tag.Draggable);
+
+				break;
+			}
+
+			default:
+				break;
+		}
+	}
+}
